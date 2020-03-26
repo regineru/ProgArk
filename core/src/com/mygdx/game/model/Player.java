5974fc4467d70cf780357c8d7405b37a6a11a8a2
jakobbod <jakobbod@stud.ntnu.no>
2020-03-26 10:57:41 +0100

    Added mvc-folders with one empty class in each

--- core/src/com/mygdx/game/model/Player.java
+++ core/src/com/mygdx/game/model/Player.java
@@ -1,4 +1,37 @@
 package com.mygdx.game.model;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+
 public class Player {
+
+    private TextureRegion texture;
+    private Vector3 position;
+    private Rectangle bounds;
+    private Boolean jump;
+
+    public Player(){
+        texture = new TextureRegion("player.png"); // TODO: need a png, placeholder
+        position = new Vector3(0,0,0); // TODO: replace placeholder values
+
+    }
+
+    public void update(float dt){
+
+    }
+
+
+    // called from controller based on input
+    public void jump(){
+        // if player positioned at "ground"
+        //      jump == true;
+
+    }
+
+
+
+    public TextureRegion getTexture(){return this.texture;} // TODO: maybe return single texture instead?
+
+
 }
