59cbb00be01b12b469d593130a685b23d2187007
fridasolheim <fridasoso@gmail.com>
2020-04-19 19:59:46 +0200

    Jaja

--- core/src/com/mygdx/game/view/MenuView.java
+++ core/src/com/mygdx/game/view/MenuView.java
@@ -2,14 +2,9 @@ package com.mygdx.game.view;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mygdx.game.ImpossibleGravity;
@@ -18,6 +13,7 @@ import com.mygdx.game.interactiveElements.HelpBtn;
 import com.mygdx.game.interactiveElements.PlayBtn;
 import com.mygdx.game.interactiveElements.QuitBtn;
 import com.mygdx.game.interactiveElements.SettingsBtn;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.hide;
 // Here we need the import of the game instance!
 
 public class MenuView extends SuperView{
@@ -46,8 +42,8 @@ public class MenuView extends SuperView{
     @Override
     public void show(){
         // Setting up the stage, adding the actors (buttons)
-        stage = new Stage(new ScreenViewport());
-        Gdx.input.setInputProcessor(stage);
+        this.stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(this.stage);
 
         // Buttons can be positioned and resized like this
         playBtn.getPlayBtn().setPosition(ImpossibleGravity.WIDTH / 2, ImpossibleGravity.HEIGHT/2+150, Align.center);
@@ -55,33 +51,24 @@ public class MenuView extends SuperView{
         helpBtn.getHelpBtn().setPosition(ImpossibleGravity.WIDTH / 2, ImpossibleGravity.HEIGHT / 2-50, Align.center);
         quitBtn.getQuitBtn().setPosition(ImpossibleGravity.WIDTH / 2, ImpossibleGravity.HEIGHT / 2-150, Align.center);
 
-        playBtn.getPlayBtn().setSize(100, 60);
+        playBtn.getPlayBtn().setSize(100, 50);
         settingsBtn.getSettingsBtn().setSize(100, 60);
         helpBtn.getHelpBtn().setSize(100, 60);
         quitBtn.getQuitBtn().setSize(100, 60);
 
+        stage.addActor(playBtn.getPlayBtn());
+        stage.addActor(settingsBtn.getSettingsBtn());
+        stage.addActor(helpBtn.getHelpBtn());
+        stage.addActor(quitBtn.getQuitBtn());
 
-        // LISTENERS FOR CLICK GESTURES (LAGGED AND WILL REMOVE BEFORE COMPLETING PROJECT
-       playBtn.getPlayBtn().addListener(new InputListener() {
+        // LISTENERS FOR CLICK GESTURES TODO: FIX BUG)
+        playBtn.getPlayBtn().addListener(new ActorGestureListener(){
             @Override
-            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                //Gdx.app.log("Example", "touch started at (" + x + ", " + y + ")");
+            public void touchDown(InputEvent event, float x, float y, int pointer, int button){
                 System.out.println("playBtn is clicked.");
                 menuController.playGamePressed();
-                return false;
+                hide();
             }
-
-            /*public boolean clicked(InputEvent e, float x, float y, int pointer, int button ) {
-                //Gdx.app.log( "listener B", "touchDown" );
-
-                return true;
-            }*/
-
-            /*@Override
-            public void clicked(InputEvent event, float x, float y)  {
-
-                //dispose();
-            }*/
         });
 
         /*playBtn.getPlayBtn().addListener(new ActorGestureListener() {
@@ -93,29 +80,32 @@ public class MenuView extends SuperView{
                 //dispose();
             }
         });*/
-        settingsBtn.getSettingsBtn().addListener(new ActorGestureListener() {
+        /*settingsBtn.getSettingsBtn().addListener(new ActorGestureListener() {
             @Override
             public void touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 super.touchDown(event, x, y, pointer, button);
                 menuController.settingsPressed();
             }
-        });
+        });*/
+
         helpBtn.getHelpBtn().addListener(new ActorGestureListener() {
             @Override
             public void touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                super.touchDown(event, x, y, pointer, button);
-                menuController.helpPressed();
+                //super.touchDown(event, x, y, pointer, button);
                 System.out.println("helpBtn is clicked.");
+                menuController.helpPressed();
+
             }
         });
-        /*quitBtn.getQuitBtn().addListener(new ActorGestureListener() {
+        /*
+        quitBtn.getQuitBtn().addListener(new ActorGestureListener() {
             @Override
             public void touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 super.touchDown(event, x, y, pointer, button);
                 System.out.println("Quit pressed");
                 menuController.quit();
             }
-        });*/
+        });
         quitBtn.getQuitBtn().addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -153,12 +143,9 @@ public class MenuView extends SuperView{
                 super.tap(event, x, y, count, button);
                 menuController.quit();
             }
-        });
+        });*/
 
-        stage.addActor(playBtn.getPlayBtn());
-        stage.addActor(settingsBtn.getSettingsBtn());
-        stage.addActor(helpBtn.getHelpBtn());
-        stage.addActor(quitBtn.getQuitBtn());
+
 
     }
 
