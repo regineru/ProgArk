85b86b79a28d41343e42d3eb68bf74c08049a302
henrikforb <henrikforb@gmail.com>
2020-04-26 12:51:39 +0200

    start on enemy

--- core/src/com/mygdx/game/view/PlayView.java
+++ core/src/com/mygdx/game/view/PlayView.java
@@ -18,6 +18,7 @@ import com.mygdx.game.controller.CharacterController;
 import com.mygdx.game.controller.ViewController;
 import com.mygdx.game.interactiveElements.MenuBtn;
 import com.mygdx.game.interactiveElements.PauseBtn;
+import com.mygdx.game.model.Enemy;
 import com.mygdx.game.model.Obstacle;
 import com.mygdx.game.model.World;
 
@@ -36,9 +37,9 @@ public class PlayView extends SuperView {
     private MenuBtn menuBtn;
     private PauseBtn pauseBtn;
 
-    public PlayView(ViewController vc){
+    public PlayView(ViewController vc, Enemy enemy){
 
-        this.world = new World();
+        this.world = new World(enemy);
         this.gameController = new GameController(vc, world);
         this.pc = new CharacterController(vc);
         this.pauseBtn = new PauseBtn();
